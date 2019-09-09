a655a4676aeaa2cbd910557f61df4d60123bb8e1
Denis-Andrei <denispeptanariu@gmail.com>
2019-09-09 15:33:17 +0100

    Updates

---
+++ precache-manifest.dccd9cce0f5ed74ac207b1412a7beebb.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b804656c484be61d2d5b004feef48791",
+    "url": "/photography-portfolio/index.html"
+  },
+  {
+    "revision": "dc47b612ea3ccd8a70d8",
+    "url": "/photography-portfolio/static/css/2.a3aa5f24.chunk.css"
+  },
+  {
+    "revision": "13cb445b5682f8cfdea9",
+    "url": "/photography-portfolio/static/css/main.5eeac6bc.chunk.css"
+  },
+  {
+    "revision": "dc47b612ea3ccd8a70d8",
+    "url": "/photography-portfolio/static/js/2.39321890.chunk.js"
+  },
+  {
+    "revision": "13cb445b5682f8cfdea9",
+    "url": "/photography-portfolio/static/js/main.51f3a776.chunk.js"
+  },
+  {
+    "revision": "be209f0386a858f23e1a",
+    "url": "/photography-portfolio/static/js/runtime~main.2960eae2.js"
+  },
+  {
+    "revision": "d85b3384fca4a3a4a21b48dced5a3f0e",
+    "url": "/photography-portfolio/static/media/card-cars.d85b3384.jpg"
+  },
+  {
+    "revision": "3baccdb7e63b391c60088c88374a01d4",
+    "url": "/photography-portfolio/static/media/card-nature.3baccdb7.jpg"
+  },
+  {
+    "revision": "8f8de2f3f4b8e9e650ae4971e43e0654",
+    "url": "/photography-portfolio/static/media/card-others.8f8de2f3.jpg"
+  },
+  {
+    "revision": "16abc4aa3bf28b389aba93c3d77068d7",
+    "url": "/photography-portfolio/static/media/card-portrait.16abc4aa.jpg"
+  },
+  {
+    "revision": "0c9646b0fa99e8886f2cfbe5d3c75f9a",
+    "url": "/photography-portfolio/static/media/card-wedding.0c9646b0.jpg"
+  },
+  {
+    "revision": "bfd08b3d6644a55031236506e720c8aa",
+    "url": "/photography-portfolio/static/media/cars-1.bfd08b3d.jpg"
+  },
+  {
+    "revision": "37a130b18dd6cbde48ca35c87cfba8d9",
+    "url": "/photography-portfolio/static/media/cars-2.37a130b1.jpg"
+  },
+  {
+    "revision": "ba2af175cb9083627133cf170a5559a2",
+    "url": "/photography-portfolio/static/media/cars-3.ba2af175.jpg"
+  },
+  {
+    "revision": "d7282533fd0ad1b8f556ba64dd6f6a2e",
+    "url": "/photography-portfolio/static/media/cars-4.d7282533.jpg"
+  },
+  {
+    "revision": "7ffeced44d80a3c744d5b8ea7ebd59bb",
+    "url": "/photography-portfolio/static/media/facebook.7ffeced4.svg"
+  },
+  {
+    "revision": "fcea5d4858374266138f5c943d4a9bf8",
+    "url": "/photography-portfolio/static/media/google-plus.fcea5d48.svg"
+  },
+  {
+    "revision": "725d7184ac37ef81a33dafb5a489c715",
+    "url": "/photography-portfolio/static/media/instagram.725d7184.svg"
+  },
+  {
+    "revision": "e1af47e02d1c9a20375369e3341e5a25",
+    "url": "/photography-portfolio/static/media/jumbo-about.e1af47e0.jpg"
+  },
+  {
+    "revision": "891a07ce05e24afdbd2bd0cebafaef06",
+    "url": "/photography-portfolio/static/media/jumbo-contact.891a07ce.jpg"
+  },
+  {
+    "revision": "58c0e9358d3993b5fedead87acc47d9a",
+    "url": "/photography-portfolio/static/media/jumbo-portofolio.58c0e935.jpg"
+  },
+  {
+    "revision": "7d1af9035cd347cc633f5d857e8b7cb6",
+    "url": "/photography-portfolio/static/media/logo.7d1af903.jpg"
+  },
+  {
+    "revision": "c5863af761d1292fe65e89974773bea7",
+    "url": "/photography-portfolio/static/media/me.c5863af7.jpg"
+  },
+  {
+    "revision": "da8d558df3e3950db845e777b5f4b9b1",
+    "url": "/photography-portfolio/static/media/nature-1.da8d558d.jpg"
+  },
+  {
+    "revision": "c3f99c6dce34aa398fa54bfabeb96ff6",
+    "url": "/photography-portfolio/static/media/nature-2.c3f99c6d.jpg"
+  },
+  {
+    "revision": "78648140daf6d44eac4dd6302837e1e5",
+    "url": "/photography-portfolio/static/media/nature-3.78648140.jpg"
+  },
+  {
+    "revision": "63c213b0662ce3f1dc6c07124963b856",
+    "url": "/photography-portfolio/static/media/nature-4.63c213b0.jpg"
+  },
+  {
+    "revision": "9f8412732916abc30b508cdc31b3dd09",
+    "url": "/photography-portfolio/static/media/others-1.9f841273.jpg"
+  },
+  {
+    "revision": "c28dc8260c99c879ab9a815acd162e1f",
+    "url": "/photography-portfolio/static/media/others-2.c28dc826.jpg"
+  },
+  {
+    "revision": "d7ac77d34d76ad216f78767fc0a38562",
+    "url": "/photography-portfolio/static/media/others-3.d7ac77d3.jpg"
+  },
+  {
+    "revision": "d45404dfad1923890bde0bf5d5edc692",
+    "url": "/photography-portfolio/static/media/portraits-1.d45404df.jpg"
+  },
+  {
+    "revision": "6e6592c66d8775929b92c6fa12f338bf",
+    "url": "/photography-portfolio/static/media/portraits-2.6e6592c6.jpg"
+  },
+  {
+    "revision": "d741b2e8a4f8eea2a83c4f53fcb84974",
+    "url": "/photography-portfolio/static/media/portraits-3.d741b2e8.jpg"
+  },
+  {
+    "revision": "3c7b7ad0df33fc7410a255038ae5604f",
+    "url": "/photography-portfolio/static/media/slide-1.3c7b7ad0.jpg"
+  },
+  {
+    "revision": "87fbb180b71cd316f29f0271cfc47d5d",
+    "url": "/photography-portfolio/static/media/slide-2.87fbb180.jpg"
+  },
+  {
+    "revision": "2844c507ba2a2948b0a2f7ee8aecbfa5",
+    "url": "/photography-portfolio/static/media/slide-3.2844c507.jpg"
+  },
+  {
+    "revision": "3188dcc328c6849d8fdd0541cf22dbf2",
+    "url": "/photography-portfolio/static/media/snapchat.3188dcc3.svg"
+  },
+  {
+    "revision": "6d569510638c9eb7cb437e098fa85812",
+    "url": "/photography-portfolio/static/media/twitter.6d569510.svg"
+  },
+  {
+    "revision": "fd5c50a98f5cdae2bddd229f9b68e187",
+    "url": "/photography-portfolio/static/media/weddings-1.fd5c50a9.jpg"
+  },
+  {
+    "revision": "48141784f7b868996a28f0741886c3b1",
+    "url": "/photography-portfolio/static/media/weddings-2.48141784.jpg"
+  },
+  {
+    "revision": "195b579f9192e66c2c1338db7a542dc0",
+    "url": "/photography-portfolio/static/media/weddings-3.195b579f.jpg"
+  },
+  {
+    "revision": "3f95627b88b678997fe107c3357811ca",
+    "url": "/photography-portfolio/static/media/weddings-4.3f95627b.jpg"
+  }
+]);
