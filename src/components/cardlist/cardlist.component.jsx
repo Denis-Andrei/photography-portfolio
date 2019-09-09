fa8d074baa891bb5bc22e4f6285a3c509a0dbfc3
Denis-Andrei <denispeptanariu@gmail.com>
2019-09-09 17:36:45 +0100

    changes

--- src/components/cardlist/cardlist.component.jsx
+++ src/components/cardlist/cardlist.component.jsx
@@ -9,12 +9,12 @@ import PortofolioCard from '../portofolioCard/portofoliocard.component';
 const CardList = ({cards}) =>{
     return(
         
-        <div className={`${window.location.href === 'http://localhost:3000/portofolio' 
-        || window.location.href === 'http://localhost:3000/portofolio/weddings' 
-        || window.location.href === 'http://localhost:3000/portofolio/nature'
-        || window.location.href === 'http://localhost:3000/portofolio/portraits' 
-        || window.location.href === 'http://localhost:3000/portofolio/others' 
-        || window.location.href === 'http://localhost:3000/portofolio/cars'  
+        <div className={`${window.location.href === 'http://localhost:3000/#/portofolio' 
+        || window.location.href === 'http://localhost:3000/#/portofolio/weddings' 
+        || window.location.href === 'http://localhost:3000/#/portofolio/nature'
+        || window.location.href === 'http://localhost:3000/#/portofolio/portraits' 
+        || window.location.href === 'http://localhost:3000/#/portofolio/others' 
+        || window.location.href === 'http://localhost:3000/#/portofolio/cars'  
 
         || window.location.href === 'https://denis-andrei.github.io/portofolio' 
         || window.location.href === 'https://denis-andrei.github.io/portofolio/weddings' 
@@ -33,7 +33,7 @@ const CardList = ({cards}) =>{
             
             cards.map( user =>{
                 
-                if(window.location.href === 'http://localhost:3000/' || window.location.href === 'https://denis-andrei.github.io' ){
+                if(window.location.href === 'http://localhost:3000/#/' || window.location.href === 'https://denis-andrei.github.io' ){
                     if((user.id % 2 !== 0) && (user.id <= 5)){
                         
                         return <Card id={user.id} title={user.title} description={user.description} side={'left'} sideImg={user.image}/>
@@ -41,7 +41,7 @@ const CardList = ({cards}) =>{
                         
                         return <Card id={user.id} title={user.title}  description={user.description} side={'right'} sideImg={user.image}/>
                      }
-                }else if(window.location.href === 'http://localhost:3000/portofolio' || window.location.href === `http://localhost:3000/portofolio/${user.title.toLowerCase()}` || (window.location.href === 'https://denis-andrei.github.io/portofolio' || window.location.href === `https://denis-andrei.github.io/portofolio/${user.title.toLowerCase()}`)){
+                }else if(window.location.href === 'http://localhost:3000/#/portofolio' || window.location.href === `http://localhost:3000/#/portofolio/${user.title.toLowerCase()}` || (window.location.href === 'https://denis-andrei.github.io/portofolio' || window.location.href === `https://denis-andrei.github.io/portofolio/${user.title.toLowerCase()}`)){
                    return <PortofolioCard id={user.id} img={user.image} title={user.title} name={user.Ptitle}/>
                 }
                 return;
